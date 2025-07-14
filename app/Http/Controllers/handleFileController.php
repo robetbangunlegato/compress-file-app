dcbd69cbd20570f3e3e4be19908f5a838aace342
robetbangunlegato <robetlegato22@mhs.mdp.ac.id>
2025-07-14 17:48:10 +0700

    update

--- app/Http/Controllers/handleFileController.php
+++ app/Http/Controllers/handleFileController.php
@@ -3,29 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 
 class handleFileController extends Controller
 {
 
 
-    /**
+    /** 
      * Store a newly created resource in storage.
      */
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $validated = $request->validate([
             'formFile' => 'required|mimes:jpg,jpeg,jpe,pdf|',
             'formSize' => 'required|integer|min:50|max:1000'
         ]);
 
-        // dd('fuck you');
-
-        // return redirect('/homepage');
+        $file = $validated['formFile'];
+        $extension = $file->getClientOriginalExtension();
+        $filename = 'original_' . now()->format('Ymd_His') . '_' . Str::random(6) . '.' . $extension;
+        $upload = $file->store($filename, 'public');
+        return ('succsess to upload!');
 
         //
     }
 
-    public function compressFile(){
-        
+    public function compressFile()
+    {
+
     }
 
 }
